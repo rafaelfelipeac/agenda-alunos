3aa47502685db11ae62654bb8f40e4ec846118b9
Rafael <rafael.felipe.sccp@gmail.com>
2016-10-04 13:47:38 -0300

    Adicionando arquivos

---
+++ app/src/main/java/com/example/rafae/agenda/ListaAlunosActivity.java
@@ -0,0 +1,33 @@
+package com.example.rafae.agenda;
+
+import android.content.Intent;
+import android.sax.StartElementListener;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+public class ListaAlunosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_alunos);
+
+        String[] alunos = {"Rafael", "Ronaldo", "Felipe", "Francisco", "Rafael", "Ronaldo", "Felipe", "Francisco", "Rafael", "Ronaldo", "Felipe", "Francisco"};
+        ListView listaAlunos = (ListView) findViewById(R.id.lista_alunos);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, alunos);
+        listaAlunos.setAdapter(adapter);
+
+        Button novoAluno = (Button) findViewById(R.id.novo_aluno);
+        novoAluno.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentVaiProFormulario = new Intent(ListaAlunosActivity.this, FormularioActivity.class);
+                startActivity(intentVaiProFormulario);
+            }
+        });
+    }
+}
